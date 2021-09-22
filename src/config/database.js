d1c8e2bf0431780321ce6de66b2d32d1da2b4c7e
GUSTAVO FERNANDES CAMPOS <gustavo.campos37@etec.sp.gov.br>
2021-09-22 02:12:09 -0300

    carga inicial API tabacaria

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'admin',
-    password: 'admin',
-    database: 'mariosdb',
+    port: 5432,
+    url: process.env.DB,
+    dialect: "postgres", // we need to implement 
     define: {
         timestamps: true,
         freezeTableName: true,
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     },
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 
 const User = require ('../models/User');
 const Product = require ('../models/Product');
 
-const connection = new Sequelize(dbConfig);
-
+console.log(process.env.DB)
+const connection = new Sequelize( //caso seja necessario executar localmente, ira modificar o que deve ser escrito
+    process.env.DB, dbConfig);
 User.init(connection);
 Product.init(connection);
 
