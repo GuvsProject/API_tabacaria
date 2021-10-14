d5b0a016bdd61d876866593f36aac015628a4706
GuvsProject <15521185+GuvsProject@users.noreply.github.com>
2021-10-13 23:24:04 -0300

    Ajustes para testes locais e integrados elephantsql

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,27 @@
+// module.exports = {
+//     dialect: 'postgres',
+//     host: 'localhost',
+//     username: 'admin',
+//     password: 'admin',
+//     database: 'mariosdb',
+//     define: {
+//         timestamps: true,
+//         freezeTableName: true,
+//     },
+// };
+
+require('dotenv').config();
+
 module.exports = {
-    dialect: 'postgres',
-    host: 'localhost',
-    username: 'admin',
-    password: 'admin',
-    database: 'mariosdb',
+    port: 5432,
+    url: process.env.DB,
+    dialect: "postgres", // we need to implement 
     define: {
         timestamps: true,
         freezeTableName: true,
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
     },
-};
+}; 
